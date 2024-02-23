43bf776f95c321e252ac4d48b20d20f54ae70289
AAKASH <71020714+aakash-6250@users.noreply.github.com>
2024-02-23 07:36:22 +0000

    first commit

--- program1.js
+++ program1.js
@@ -3,7 +3,17 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let ans=true;
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i+1; j < s.length; j++) {
+            if(s.charAt(i)===s.charAt(j)){
+                continue
+            }
+
+            
+        }
+        
+    }
 };
 
 module.exports = { isValid };
